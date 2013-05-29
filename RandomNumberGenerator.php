f7df5953b552c302b8fbff9101cca6decf784789
Fernando Palomo Garcia <fernando@bluejay.es>
2013-05-29 16:37:24 +0200

    Tests en rojo

--- RandomNumberGenerator.php
+++ RandomNumberGenerator.php
@@ -13,6 +13,7 @@ class RandomNumberGenerator
 
     public function between($lower, $higher)
     {
+
         return array(1);
 
     }
--- RandomNumberGeneratorTest.php
+++ RandomNumberGeneratorTest.php
@@ -15,15 +15,46 @@ require_once './vendor/phpunit/phpunit/PHPUnit/Autoload.php';
 class RandomNumberGeneratorTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testGenerate()
+    // test con un solo elemento
+    public function testWithOneElement()
     {
         $generateRandomNumbers = new RandomNumberGenerator();
         $resultBetween1and1 = $generateRandomNumbers->between(1, 1);
-
         $this->assertTrue(is_array($resultBetween1and1));
         $this->assertContains(1, $resultBetween1and1);
-        $this->assertEquals(1,count($resultBetween1and1));
+        $this->assertEquals(1, count($resultBetween1and1));
+    }
 
+    // test con un solo elemento diferente de 1 ( evitar problemas tontos de código )
+    public function testWithOneElementDifferentFrom1()
+    {
+        $generateRandomNumbers = new RandomNumberGenerator();
+        $resultBetween4and4 = $generateRandomNumbers->between(4, 4);
+        $this->assertTrue(is_array($resultBetween4and4));
+        $this->assertContains(4, $resultBetween4and4);
+        $this->assertEquals(1, count($resultBetween4and4));
+
+    }
+
+    // test con varios elementos
+    public function testWithSeveralElements()
+    {
+        $generateRandomNumbers = new RandomNumberGenerator();
+        $resultBetween3and10 = $generateRandomNumbers->between(3, 10);
+        $this->assertTrue(is_array($resultBetween3and10));
+        for ($i = 3; $i <= 10; ++$i) {
+            $this->assertContains($i, $resultBetween3and10);
+        }
+        $this->assertEquals(8, count($resultBetween3and10));
+    }
+
+    // test que debe fallar , ya que no hay elementos mayores que 7 y menores que 2
+    public function testWithoutElements()
+    {
+        $generateRandomNumbers = new RandomNumberGenerator();
+        $resultBetween3and1 = $generateRandomNumbers->between(7, 2);
+        $this->assertTrue(is_array($resultBetween3and1));
+        $this->assertEquals(0, count($resultBetween3and1));
 
     }
 
