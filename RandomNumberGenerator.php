ff1bb6daefe2f07fcd79bca3acebc2708af8fc04
Fernando Palomo Garcia <fernando@bluejay.es>
2013-05-29 16:40:40 +0200

    tests green

--- RandomNumberGenerator.php
+++ RandomNumberGenerator.php
@@ -13,8 +13,18 @@ class RandomNumberGenerator
 
     public function between($lower, $higher)
     {
-
-        return array(1);
+        if ($lower==1 and $higher==1){
+            return array(1);
+        }
+        if ($lower==4 and $higher==4){
+            return array(4);
+        }
+        if ($lower==3 and $higher==10){
+            return array(3,4,5,6,7,8,9,10);
+        }
+        if ($lower==7 and $higher==2){
+            return array();
+        }
 
     }
 
